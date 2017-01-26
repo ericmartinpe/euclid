91af9e0c03e08cd7d804a4805516fa3513fef391
Peter Ellis <peter.ellis@bigladdersoftware.com>
2017-01-25 18:54:48 -0700

    Add developer hook file to allow testing from Git working copy.

---
+++ euclid_developer_hook.rb
@@ -0,0 +1,13 @@
+# Copyright (c) 2017 Big Ladder Software LLC. All rights reserved.
+# See the file "license.txt" for additional terms and conditions.
+
+####################################################
+# COPY THIS FILE TO THE SKETCHUP PLUGINS DIRECTORY #
+####################################################
+
+SKETCHUP_CONSOLE.show
+
+# EDIT THIS PATH TO POINT TO YOUR WORKING COPY OF THE REPOSITORY.
+$LOAD_PATH << "C:/Projects/Euclid/euclid-for-sketchup~/extension"
+
+load("euclid.rb")
